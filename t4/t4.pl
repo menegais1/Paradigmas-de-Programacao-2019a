b089a134921496b4a5e2b431e2fe71b6325f1dc1
menegais1 <menegais1@gmail.com>
2019-05-05 20:35:53 -0300

    First t4 commit

---
+++ t4/t4.pl
@@ -0,0 +1,80 @@
+relacionamento(anita,bernado).
+relacionamento(bernardo,caren).
+relacionamento(anita,pedro).
+relacionamento(pedro,alice).
+relacionamento(alice,henrique).
+relacionamento(henrique,bia).
+relacionamento(maria,adriano).
+relacionamento(adriano,caren).
+
+
+ciume(anita,alice).
+ciume(anita,caren).
+
+pobre(pedro).
+pobre(maria).
+pobre(bia).
+pobre(bernardo).
+
+rico(alice).
+rico(henrique).
+rico(adriano).
+rico(caren).
+
+insano(adriano).
+insano(maria).
+
+santaMaria(pedro,segunda).
+santaMaria(pedro,terca).
+santaMaria(pedro,quinta).
+santaMaria(bernardo,segunda).
+santaMaria(bernardo,terca).
+santaMaria(bernardo,quinta).
+santaMaria(caren,quinta).
+santaMaria(bia,quinta).
+santaMaria(adriano,quarta).
+santaMaria(maria,terca).
+santaMaria(maria,quarta).
+santaMaria(maria,quinta).
+
+portoAlegre(caren,segunda).
+portoAlegre(caren,terca).
+portoAlegre(caren,quarta).
+portoAlegre(pedro,quarta).
+portoAlegre(bernardo,quarta).
+portoAlegre(henrique,terca).
+portoAlegre(bia,terca).
+portoAlegre(bia,quarta).
+portoAlegre(alice,terca).
+portoAlegre(alice,quarta).
+
+apartamento(pedro,sexta).
+apartamento(caren,sexta).
+apartamento(henrique,segunda).
+apartamento(henrique,quarta).
+apartamento(henrique,quinta).
+apartamento(henrique,sexta).
+apartamento(bia,segunda).
+apartamento(bia,sexta).
+apartamento(adriano,quinta).
+apartamento(adriano,sexta).
+apartamento(alice,segunda).
+apartamento(alice,quinta).
+apartamento(alice,sexta).
+apartamento(bernardo,sexta).
+apartamento(maria,segunda).
+apartamento(maria,sexta).
+
+motivacao(P) :- pobre(P);insano(P);ciume(anita,P).
+
+rouboBastao(P) :- portoAlegre(P, quinta);santaMaria(P, quarta).
+
+rouboMartelo(P) :- apartamento(P, quarta);apartamento(P, quinta).
+
+armaCrime(P) :- rouboBastao(P);rouboMartelo(P).
+
+rouboChave(P) :- santaMaria(P,segunda);portoAlegre(P, terca).
+
+acesso(P) :- armaCrime(P), rouboChave(P), (apartamento(P,quinta); apartamento(P,sexta)).
+
+assassino(P) :- motivacao(P), acesso(P).
